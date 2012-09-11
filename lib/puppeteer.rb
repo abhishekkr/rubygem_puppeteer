a2cd8351abafc501381ec00b353abc4a4ead559f
AbhishekKr <abhikumar163@gmail.com>
2012-09-11 07:44:03 +0530

    some code refactoring for inclusion of templates for manifest

--- lib/puppeteer.rb
+++ lib/puppeteer.rb
@@ -3,19 +3,33 @@ require "puppeteer/screenplay"
 
 module Puppeteer
 
-  def self.module(namespace, module_path = File.expand_path(File.dirname __FILE__))
+  def self.module_structure
+    ['files', 'lib', 'spec','templates', 'tests']
+  end
+
+  def self.module(namespace,
+      module_path = File.expand_path(File.dirname __FILE__),
+      dir_struct  = module_structure
+    )
     dirs = []
     dirs << File.join(module_path, namespace.split(/::/)[0])
     dirs << File.join(dirs[0], 'manifests')
     namespace.split(/::/)[1..-2].each_with_index do |nam, idx|
-      dirs << File.join(dirs[idx], nam)
+      dirs << File.join(dirs[idx+1], nam)
     end
-    dirs.each {|dir| Dir.mkdir dir }
-    if dirs.size < 3
+    dirs.each {|dir| Dir.mkdir dir unless File.directory?(dir) }
+    dir_struct.each {|mdir|
+      dir = File.join(dirs[0], mdir)
+      Dir.mkdir dir unless File.directory?(dir)
+    }
+
+    unless namespace.match(/::/)
       init_pp = File.join dirs[-1], 'init.pp'
     else
-      init_pp = File.join dirs[-1], namespace.split(/::/)[-1]
+      puts File.join(dirs[-1], namespace.split(/::/)[-1])
+      init_pp = "#{File.join dirs[-1], namespace.split(/::/)[-1]}.pp"
     end
-    Screenplay.create_pp init_pp
+
+    Screenplay.create_pp namespace, init_pp
   end
 end
--- lib/puppeteer/screenplay.rb
+++ lib/puppeteer/screenplay.rb
@@ -1,16 +1,26 @@
-module Screenplay
+require 'erb'
 
-  def self.pp_data(pp_name, pp_config = nil)
-    pp_config ||= "# module :: #{pp_name.split(/::/).join(' > ')}"
-    return <<-PP_DATA
-class #{pp_name} {
-  #{pp_config}
-}
-      PP_DATA
-  end
+module Puppeteer
+  module Screenplay
+    attr_accessor :pp_name, :pp_config
 
-  def self.create_pp(fylname, fyldata = nil)
-    fyldata ||= pp_data
-    File.open(fylname, 'w') {|fyl| fyl.write(fyldata) }
+    def self.erb(fylname)
+      dir = File.dirname(File.expand_path(__FILE__))
+      file_erb = File.join dir, 'templates', fylname
+      template = File.open(file_erb, 'r').read
+      ERB.new(template)
+    end
+
+    def self.default_manifest
+      @pp_config ||= "# module :: #{@pp_name.split(/::/).join(' > ')}"
+      manifest_dat = erb('manifests_pp.erb').result(binding)
+      return manifest_dat
+    end
+
+    def self.create_pp(namespace, fylname)
+      @pp_name = namespace
+      fyldata = default_manifest
+      File.open(fylname, 'w') {|fyl| fyl.write(fyldata) }
+    end
   end
 end
